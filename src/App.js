6ff36aae9bf3d15e712e61d5d836fb6404939664
JohnDanielTejero <96475894+JohnDanielTejero@users.noreply.github.com>
2022-10-21 10:44:46 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+
+const formSubmit = (e) => {
+  e.preventDefault();
+  fetch('http://localhost:8000/api/register', {
+
+    "method": "POST",
+
+    "body": JSON.stringify({
+      "name" : e.target[0].value,
+      "email" : e.target[1].value,
+      "password" : e.target[2].value
+    }),
+
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    
+  }).then(response => response.json())
+  .then(e => {
+    if(e.status === 'success')localStorage.setItem('token', e.authorisation.token)
+  }).catch(e=>console.log(e));
+}
+
+function currentUser(e){
+  e.preventDefault();
+  fetch('http://localhost:8000/api/get-user', {
+    'method' : 'GET',
+    headers : {
+      'Authorization' : 'Bearer' + localStorage.getItem('token'),
+      "Accept": "application/json",
+    }
+  }).then(res => res.json()).then(e=>console.log(e));
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={formSubmit}>
+            <input placeholder='name'/>
+            <input placeholder='email'/>
+            <input placeholder='password'/>
+            <button>tite</button>
+        </form>
+      </div>
+      <button onClick={currentUser}>Click me</button>
     </div>
   );
 }
